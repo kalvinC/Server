53154d324dbafe565dffdf39f2038c1709b416f5
kalvinC <lklzyy@163.com>
2014-12-28 08:32:57 +0800

    add note

--- communication/tcpserver.h
+++ communication/tcpserver.h
@@ -11,13 +11,51 @@ public:
 	~CTcpServer();
 
 public:
+	/**
+	@return if set success, true, else false.
+	*/
 	bool SetNonBlocking();
 	
+	/**
+	@brief wrapper the bind function of socket.The ip address
+	is 127.0.0.1 and the default port is 9000.
+	@return if bind successful, return true, else false.
+	*/
 	bool Bind();
+	/**
+	@brief default listen 5.
+	@return if listen succussful, return true.else false.
+	*/
 	bool Listen();
+	/**
+	@brief wrapper the function accept of socket, and accept return the connecting 
+	client socekt and client address.
+	@param[out] client_socket
+	@param[out] client_socket_addr
+	@return if has the client connecting to, true else false.
+	*/
 	bool Aceept(int & client_socket, struct sockaddr &client_socket_addr);
+	/**
+	@brief wrapper the function accept, and just return client socket.
+	@param[out] client_socket.
+	@return if has the client connecting to, true else false.
+	*/
 	bool Aceept(int & client_socket);
+	/**
+    @brief send data to client.
+    @param[in] client_socket client socket.
+    @param[in] buffer data need to be sent.
+    @param[in] buffer_size how much data need to be sent.
+    @return how much data has been sent successfully.
+	*/
 	int SendData(int client_socket, const char * buffer, unsigned int buffer_size);
+	/**
+	@breif recv data from client socket.
+	@param[in] client_socket client socket.
+	@param[out] buffer store data from client socket.
+	@param[in] buffer_size size of buffer.
+	@return how much data has been recieve successfully. 
+	*/
 	int RecvData(int client_socket, char * buffer, unsigned int buffer_size);
 
 private:
