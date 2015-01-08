a18268231d0c6590d6380ec96a539291b59ac3e0
kalvinC <lklzyy@163.com>
2015-01-08 22:33:50 +0800

    implements kqueuemodel

--- communication/kqueuemodel.h
+++ communication/kqueuemodel.h
@@ -5,20 +5,37 @@
 class KQueueModel
 {
 public:
-	typedef enum Socket_Event{
-		Event_Read = 0,
-		Event_Write
-	  }Socket_Event;
-
+	/**
+	* As know, the max count of socket is 65532, so we set the max event count 65532.
+	* when you construct a kqueuemodel with a count parameter, if the count is over 65532, 
+	* the max event count will be set to 65532.
+	*/
 	KQueueModel();
+	KQueueModel(unsigned int max_event_count);
 	~KQueueModel();
 
-	bool AddEvent(int socket_fd, Socket_Event event);
-	bool DeleteEvent(int socket_fd);
+    /**
+    *@brief register socket on kqueue.And we mainly focus the read event.
+    *@param[in] socket id 
+    *@return if register socket on kqueue successfully, true else false.
+    */
+    bool Register(int socket_fd);
+    /**
+    *@brief unregister socket on kqueue. And we mainly focus the read event.
+    *@param[in] socket id 
+    *@return if unregister socket on kqueue successfully, true else false.
+    */
+    bool UnRegister(int socekt_fd);
 
-
+    /**
+    *@brief get the response list on kqueue.
+    *@param[out] kevent_list kevent list
+    *@return the number of response kevent on kqueue. 
+    */
+    int EventResponseList(struct kevent * kevent_list);
 
 private:
-	int kqueue_fd;
+	int kqueue_fd;/* kqueue id*/
+	unsigned int max_event_count; /* the max kevent count*/
 };
 #endif
--- communication/kqueuemodel_impl.cpp
+++ communication/kqueuemodel_impl.cpp
@@ -1,10 +1,34 @@
 #include "kqueuemodel.h"
 #include "sockconfig.h"
 #include <unistd.h>
+#include <stdio.h>
+#include <string>
+#include <assert.h>
 
 KQueueModel::KQueueModel()
 {
 	kqueue_fd = kqueue();
+
+	if(kqueue_fd == -1)
+	{
+		printf("%s\n", strerror(errno));
+	}
+
+	max_event_count = 65532;
+}
+
+KQueueModel::KQueueModel(unsigned int max_event_count)
+{
+	if(max_event_count > 65532)
+		this->max_event_count = 65532;
+
+	this->max_event_count = max_event_count;
+	kqueue_fd = kqueue();
+
+	if(kqueue_fd == -1)
+	{
+		printf("%s\n", strerror(errno));
+	}
 }
 
 KQueueModel::~KQueueModel()
@@ -13,12 +37,46 @@ KQueueModel::~KQueueModel()
 		close(kqueue_fd);
 }
 
-bool KQueueModel::AddEvent(int socket_fd, Socket_Event event)
+bool KQueueModel::Register(int socket_fd)
 {
-	return false;
+	struct kevent kq_event;
+	EV_SET(&kq_event, socket_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+
+	int ret = kevent(kqueue_fd, &kq_event, 1, NULL, 0, NULL);
+	if(ret == 1)
+	{
+		printf("%s\n", strerror(errno));
+		return false;
+	}
+
+	return true;
 }
 
-bool KQueueModel::DeleteEvent(int socket_fd)
+bool KQueueModel::UnRegister(int socket_fd)
 {
-	return false;
+	struct kevent kq_event;
+	EV_SET(&kq_event, socket_fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+
+	int ret = kevent(kqueue_fd, &kq_event, 1, NULL, 0, NULL);
+	if(ret == -1)
+	{
+		printf("%s\n", strerror(errno));
+		return false;
+	}
+
+	return true;
+}
+
+int KQueueModel::EventResponseList(struct kevent * kevent_list)
+{
+	assert(kevent_list != NULL);
+
+	int ret = kevent(kqueue_fd, NULL, 0 , kevent_list, max_event_count, NULL);
+	
+	if(ret == -1)
+	{
+		printf("%s\n", strerror(errno));
+	}
+	
+	return ret;
 }
