71aa30537073628a91d62b3d9902052da0ee2b40
kalvinC <lklzyy@163.com>
2015-03-05 23:39:48 +0800

    definet interface function for database client

--- database/dbclient.h
+++ database/dbclient.h
@@ -1,10 +1,13 @@
 #ifndef DBCLIENT_H
 #define DBCLIENT_H
 
+#include <string>
+
+
 class DBClient
 {
  public:
-   DBClient();
-   ~DBClient();
-}
+   virtual bool Connect(std::string username, std::string password = "") = 0;
+   virtual bool Disconnect() = 0;
+};
 #endif 
