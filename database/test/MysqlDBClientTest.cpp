ff8d62a96e9281aa75193636142340b1fb4c10e4
kalvinC <lklzyy@163.com>
2016-02-02 15:18:08 +0800

    test MysqlDBClinet.cpp and fix the vistor container privillege to public

--- database/test/MysqlDBClientTest.cpp
+++ database/test/MysqlDBClientTest.cpp
@@ -14,6 +14,17 @@ int main()
         for(int i =0; i < mysqldbclient.col_name_vector.size(); i++)
 	{
 		printf("%s\n", mysqldbclient.col_name_vector[i].c_str());
-	}		
+	}	
+
+	std::map<int, std::vector<std::string> >::iterator iter = mysqldbclient.row_val_map.begin();
+	for(;iter != mysqldbclient.row_val_map.end(); iter++)
+	{
+		printf("row num %d:", iter->first);
+		for(int i = 0; i < iter->second.size(); i++)
+		{
+			printf("%s ", iter->second[i].c_str());
+		}
+		printf("\n");
+	}	
 	return 0;
 }
